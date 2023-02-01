945f926d6a2459e8a9167dd4d883a369b2533ad2
YuliannC <yuliannaperez1707@gmail.com>
2023-02-01 17:15:23 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Contador from './assets/Contador'
+
+function App() {
+ 
+  return(
+    <>
+    <Contador/>
+    </>
+  )
+
+
+}
+
+export default App
